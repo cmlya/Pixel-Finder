c5d5d9304574cf1fac354e0fb438e5fdb7dceec8
camillea <83574726+cmlya@users.noreply.github.com>
2021-08-10 22:28:20 -0400

    Clicked color is random

--- src/myPanel.java
+++ src/myPanel.java
@@ -4,11 +4,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Random;
 
 public class myPanel extends JPanel implements ActionListener {
     final int SCREEN_WIDTH = 250, SCREEN_HEIGHT = 150;
-    Color backgroundColor = new Color(0, 106, 213), clickedColor = new Color(23, 234, 52);
+    Color backgroundColor = new Color(0, 106, 213);
+    // Color clickedColor = new Color(23, 234, 52);
     Color labelColor = new Color(248, 182, 255);
+    Random rnd = new Random();
 
 
     myPanel() {
@@ -19,8 +22,9 @@ public class myPanel extends JPanel implements ActionListener {
             @Override
             public void mousePressed(MouseEvent e) {
                 Graphics g = getGraphics();
-                g.setColor(clickedColor);
-                System.out.println(e.getXOnScreen() + ", " + e.getYOnScreen());
+                // g.setColor(clickedColor);
+                g.setColor(new Color(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
+                System.out.println(e.getXOnScreen() * 2 + ", " + e.getYOnScreen() * 2);
                 g.fillOval(e.getX(), e.getY(), 15, 15);
                 g.setColor(labelColor);
                 g.drawString(e.getXOnScreen() * 2 + ", " + e.getYOnScreen() * 2, e.getX(), e.getY());
