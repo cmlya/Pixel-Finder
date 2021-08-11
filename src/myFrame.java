c4a2323057b8f2476cd55370eb3835938087c959
camillea <83574726+cmlya@users.noreply.github.com>
2021-08-10 22:46:48 -0400

    Added icon

--- src/myFrame.java
+++ src/myFrame.java
@@ -7,6 +7,7 @@ public class myFrame extends JFrame {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(true);
         this.pack();
+        this.setIconImage(new ImageIcon("Icon.png").getImage());
         this.setVisible(true);
         this.setLocationRelativeTo(null);
     }
